6082e5233594053bd1efd8cefd9f108d0720a685
rrgayhart <rrgayhart@gmail.com>
2017-04-03 17:25:33 -0600

    inital set up and controller

---
+++ app/controllers/presidents_controller.rb
@@ -0,0 +1,2 @@
+class PresidentsController < ApplicationController
+end
---
+++ test/controllers/presidents_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PresidentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
